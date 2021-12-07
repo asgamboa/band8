93be25c228dfffd842691e6602b258b27e5bbfc8
Asgamboa12 <33297469+Asgamboa12@users.noreply.github.com>
2021-12-06 23:52:10 -0700

    [ADD] Added doc folder

---
+++ docs/_next/static/OKvKJoRbTKZhFkRsH1GUg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/947-2ccd73fa04041500.js","static/css/f2a62999486f96a3.css","static/chunks/pages/index-a37a47af23fbc273.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

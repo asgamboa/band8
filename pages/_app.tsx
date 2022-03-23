1eb80a2c6b29e6d2b0fa4cbbaf1175a5574a024c
Andrés <33297469+asgamboa@users.noreply.github.com>
2022-03-22 22:33:17 -0700

    [ADD] CssBaseline

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { StyledEngineProvider } from "@mui/styled-engine";
+import { CssBaseline } from "@mui/material";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       </Head>
+      <CssBaseline />
       <Component {...pageProps} />
     </StyledEngineProvider>
   );
